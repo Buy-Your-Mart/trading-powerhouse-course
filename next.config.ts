bacfeb7c2bef9eb7fbe086ee4529c4702f3a5142
Vishwas Moorjani <vishwasmoorjani@gmail.com>
2026-01-08 10:14:56 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,19 @@
 import type { NextConfig } from "next";
 import path from "node:path";
 
-const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
+const LOADER = path.resolve(__dirname, "src/visual-edits/component-tagger-loader.js");
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" }
     ],
   },
-  outputFileTracingRoot: path.resolve(__dirname, '../../'),
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   }
 };
 
 export default nextConfig;
-// Orchids restart: 1767825020056
